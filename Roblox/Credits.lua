69fa15addf3da75cebd19604b0da90cc1de7ab33
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2023-01-21 12:22:02 -0700

    Create Credits.lua

---
+++ Roblox/Credits.lua
@@ -0,0 +1,13 @@
+rconsoleprint("\n\n\n\n\n\n\n")
+
+rconsoleprint("                          _____     _ _        __              _       _    _____     _ \n")
+
+rconsoleprint("                         |   __|_ _| | |_ _   |  |   ___ ___ _| |___ _| |  |   __|_ _|_|\n")
+
+rconsoleprint("                         |   __| | | | | | |  |  |__| . | .'| . | -_| . |  |  |  | | | |\n")
+
+rconsoleprint("                         |__|  |___|_|_|_  |  |_____|___|__,|___|___|___|  |_____|___|_|\n")
+
+rconsoleprint("                                       |___|                                                                                                          \n\n")
+
+rconsoleprint("                                      Github - github.com/SeasonalKirito\n                                            Scriptblox - Seasonal\n                                           Discord - SEASONAL#1235")
